fde5032b3f1459512fe9299573370bcc46a44ba9
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-03 00:19:41 +0200

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+            print("{:c}".format(ord(ch) - 32), end="")
+        else:
+            print("{:c}".format(ord(ch)), end="")
+    print()
