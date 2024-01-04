fbbfbf3efda1241575e042235ff900fd1e2193d5
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-05 00:16:16 +0200

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     print("{:d} arguments".format(len(sys.argv) - 1), end="")
     if (len(sys.argv) - 1 == 0):
-        print(".")
+        print("s.")
     else:
         print(":")
         for i in range(1, len(sys.argv)):
