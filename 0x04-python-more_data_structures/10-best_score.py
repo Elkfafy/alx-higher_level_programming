ee6d87c364c58dfcb4a56258ef6a10bd8d73fa83
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-10 11:37:13 +0200

    11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     mx_key = None
     for key in a_dictionary.keys():
-        if mx_key == None:
+        if mx_key is None:
             mx_key = key
         if a_dictionary[mx_key] < a_dictionary[key]:
             mx_key = key
