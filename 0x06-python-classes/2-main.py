e129eec35517e37cefa99cc1f337c935150cdcb6
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-03 19:13:38 +0200

    2

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 Square = __import__('2-square').Square
+print(type(3))
 my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
@@ -16,13 +17,13 @@ except Exception as e:
         print(e)
 try:
         my_square_3 = Square("3")
-            print(type(my_square_3))
-                print(my_square_3.__dict__)
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
 except Exception as e:
         print(e)
 try:
         my_square_4 = Square(-89)
-            print(type(my_square_4))
-                print(my_square_4.__dict__)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
 except Exception as e:
         print(e)
