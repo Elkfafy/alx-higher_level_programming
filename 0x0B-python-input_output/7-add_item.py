3660798efd4fa9b577db4d4b587ef8a3032d7360
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-06 16:07:09 +0200

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,9 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
-obj = load_from_json_file(filename)
+try:
+    obj = load_from_json_file(filename)
+except FileNotFoundError:
+    obj = []
 obj += argv[1:]
 save_to_json_file(obj, filename)
