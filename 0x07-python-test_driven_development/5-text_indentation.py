637ff08ee35505df21631e6610945f8032c5b6ea
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-14 16:21:54 +0200

    5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module
+"""
+
+
+def text_indentation(text):
+    """ text indentation function """
+
+    if (not isinstance(text, str)):
+        raise TypeError("text must be a string")
+    for ch in text:
+        if ch in ['.', '?', ':']:
+            print('\n')
+        else:
+            print(ch, end='')
