dc57e7970dc02af74a6f79c18ce868e92abe639b
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-14 17:17:41 +0200

    6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -57,4 +57,4 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty(self):
         """ test empty """
 
-        self.assertEqual(max_integer([1]), None)
+        self.assertEqual(max_integer([1]), 1)
