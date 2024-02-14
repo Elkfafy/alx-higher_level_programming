2d02a552577c8e4a6ced92f087963101073039af
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-14 16:28:41 +0200

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,10 +7,15 @@ module
 def text_indentation(text):
     """ text indentation function """
 
+    start = True
     if (not isinstance(text, str)):
         raise TypeError("text must be a string")
     for ch in text:
         if ch in ['.', '?', ':']:
             print(ch + '\n')
+            start = True
         else:
-            print(ch, end='')
+            if (start == False or ch not in [' ', '\t']):
+                print(ch, end='')
+                continue
+            start = False
