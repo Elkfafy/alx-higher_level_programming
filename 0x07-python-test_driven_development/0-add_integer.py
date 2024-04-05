6fd2d2559fcc8810855b7ad19a73513cdc5fd0da
root <root@DESKTOP-H5E3Q8S>
2024-04-05 10:49:28 +0200

    cohort 22

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-module
-"""
+'''
+    python module for adding two integers
+'''
 
 
 def add_integer(a, b=98):
-    """ add_integer function """
+    """ add_integer """
 
-    if (not isinstance(a, int) and not isinstance(a, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
