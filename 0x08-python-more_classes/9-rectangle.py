3bbd4da642cb80ef11f20a3871c91f06a96fbc70
elkfafy <hmdeslam.adel@gmail.com>
2024-04-09 10:23:53 +0200

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
             initialize the rectangle
         """
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
